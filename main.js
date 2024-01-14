c15410960a44bcaf516383bf5b71d5c9b10ec235
Shanie <shanieportal@gmail.com>
2024-01-14 08:44:20 -0500

    JS Functionality

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 let input = [];
 let output = document.getElementById('output');
 
-console.log("JavaScript file connected!");
+let operator = null;
+let nextOperator = null;
+let currentOperand = 0;
+let nextOperand = 0;
 
 
 function clearDisplay() {
     input = [];
+    updateDisplay();
+    operator = null;
+}
+
+function backspace() {
+    input.pop();
     updateDisplay();
 }
 
@@ -14,26 +23,48 @@ function updateDisplay() {
 }
 
 function appendDisplay(val) {
+    if (input.length === 0 && val === 0) {
+        return;
+    }
     input.push(String(val));
     updateDisplay();
 }
 
-function add() {
+
+
+
+
+function setOperation(op) {
     let total = 0;
-
-    for (let i = 0; i < input.length; i++) {
-        total += input[i];
+    if (operator !== null) {
+        nextOperator = op;
+        total = calculate();
+        input = [total.toString()];
+        updateDisplay();
+        operator = nextOperator;
+        input = [];
     }
-
-    return total;
+    else {
+        operator = op;
+        currentOperand = parseFloat(input.join(''));
+        input = [];
+    }
 }
 
-function subtract() {
-    let total = 0;
 
-    for (let i = 0; i < input.length; i++) {
-        total -= input[i];
+
+function calculate() {
+    let nextOperand = parseFloat(input.join(''));
+    switch (operator) {
+        case '+':
+            return currentOperand + nextOperand;
+        case '-':
+            return currentOperand - nextOperand;
+        case 'x':
+            return currentOperand * nextOperand;
+        case '÷':
+            return currentOperand / nextOperand;
+        default:
+            return;
     }
-
-    return total;
 }
